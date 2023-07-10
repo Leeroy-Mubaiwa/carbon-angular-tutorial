62ce68f36076751c5803a39ed25897218a146738
MorabaTech <50992477+MorabaTech@users.noreply.github.com>
2023-07-10 10:10:49 +0200

    another

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IconModule, UIShellModule } from 'carbon-components-angular';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UIShellModule,
+    IconModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
